abf1bc9da4aefdacbddf16949295642fa00f0a82
leeky-mem <116338492+leeky-mem@users.noreply.github.com>
2023-11-15 18:08:14 +0100

    Added input argument

--- intel_hex_file_cecksum_calc.c
+++ intel_hex_file_cecksum_calc.c
@@ -19,21 +19,66 @@ where:
   01h + NOT(02h + 00h + 00h + 04h + FFh + FFh).
 */
 
+
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define len 20
+#define INTEL_HEX_MAX_LINE_LEN 42
+#define INTEL_HEX_MIN_LINE_LEN 2
 
-// one line of an intel hex file without leading colon and cecksum (last byte)
-static uint8_t val[len]= {0x10,0x06,0x20,0x00,0x2C,0xD9,0x61,0x19,0x08,0xF1,0xFF,0x33,0x04,0xD2,0x8E,0x45,0x02,0xD9,0xA8,0xF1};
-static uint8_t cc=0;
-
-int main (void)
+int main (int argc, char *argv[])
 {
-	for (int i =0; i < len; i++){
+	uint8_t cc = 0;
+	int line_len = strlen(argv[1]);
+	int byte_len = line_len / 2;
+	char *input;
+	char temp[3];
+	temp[2] = 0;
+	int j = 0;
+	uint8_t val[byte_len];
+
+	// Input check
+	if (argc == 2) {
+		input = argv[1];
+		if (line_len > INTEL_HEX_MAX_LINE_LEN){
+			printf("Input line cannot be longer than 21 Byte!\n");
+			return -1;
+		}
+		if (line_len < INTEL_HEX_MIN_LINE_LEN){
+			printf("Input line cannot be shorter than 2 byte!");
+			return -1;
+		}
+	}
+	else if (argc > 2) {
+		printf("Too many arguments supplied.\n");
+		return -1;
+	}
+	else {
+		printf("One argument expected.\n");
+		return -1;
+	}
+
+	//Convert input string to array of baytes
+	for (int i = 0; i < line_len ; i+=2){
+		temp[0] = input[i];
+		temp[1] = input[i + 1];
+		val[j] = (uint8_t)strtol(temp, NULL, 16);
+		//printf("temp = 0x%s\n", temp);
+		//printf("val %d = 0x%02x\n",j ,val[j]);
+		j += 1;
+	}
+
+	// Calculate Checksum
+	// byte_len - 1 because the last byte is the old checksum
+	for (int i = 0; i < byte_len - 1; i++){
 		cc += val[i];
 	}
 	cc= 0x01 + ~(cc);
-	printf("cc= 0x%02x", cc);
-  	return 0;
+
+	// Show results;
+	printf("Calculated cc = 0x%02x\n", cc);
+	printf("Provided cc = 0x%02x\n", val[byte_len - 1]);
+	return 0;
 }
